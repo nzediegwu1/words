70bb9cb612b5f1d6d4e582ec3876fb894d983711
nzediegwu1 <nzediegwu1@outlook.com>
2017-11-20 19:05:00 +0100

    Refractor algorithm to eliminate set usage

--- words.js
+++ words.js
@@ -1,26 +1,17 @@
 ﻿exports.words = sentence => {
     if (typeof sentence === 'string') {
         const inputArray = sentence.toLowerCase().split(' ');
-        const inputSet = new Set(inputArray);
         const outputObject = {};
-        for (let item of inputSet) {
-            if (item !== '') {
-                outputObject[item] = 1;
-            }
-        }
-        const newSet = new Set();
-        let tracker = 0; // used to keep track of newSet size
         for (let i = 0; i < inputArray.length; i++) {
             if (inputArray[i] !== '') {
-                newSet.add(inputArray[i]);
-                tracker++;
-                if (newSet.size !== tracker) {
-                    outputObject[inputArray[i]]++; // increment object item
-                    tracker--;
+                if (outputObject[inputArray[i]] === undefined) {
+                    outputObject[inputArray[i]] = 1;
+                } else {
+                    outputObject[inputArray[i]]++;
                 }
             }
         }
         return outputObject;
     }
-    return { 'error': 'parameter must be a string' };
+    return { error: 'parameter must be a string' };
 };
