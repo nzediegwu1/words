a7a45ba008c12fbb6c233c98d5573824e406436e
nzediegwu1 <nzediegwu1@outlook.com>
2017-11-20 20:03:58 +0100

    removed redundant tests
    *rewrote tests to handle new validations in words function

--- test/test.js
+++ test/test.js
@@ -5,47 +5,45 @@ const assert = chai.assert;
 chai.config.truncateThreshold = 0;
 describe('Test words function', () => {
     it('should handle specified input', () => {
-        let testcase = JSON.stringify(program.words('olly olly in come free'));
-        assert.equal(testcase, JSON.stringify({ olly: 2, in: 1, come: 1, free: 1 }));
+        let testcase = program.words('olly olly in come free');
+        assert.deepEqual(testcase, { olly: 2, in: 1, come: 1, free: 1 });
     });
-
     it('should handle more complex input', () => {
-        let testcase = JSON.stringify(program.words('olly olly has come in the free has the free olly'));
-        assert.equal(testcase, JSON.stringify({ 'olly': 3, 'has': 2, 'come': 1, 'in': 1, 'the': 2, 'free': 2 }));
+        let testcase = program.words('olly olly has come in the free has the free olly');
+        assert.deepEqual(testcase, { 'olly': 3, 'has': 2, 'come': 1, 'in': 1, 'the': 2, 'free': 2 });
     });
     it('should return an object', () => {
         let testCase = typeof program.words('olly olly has come in the free has the free olly');
-        assert.equal(testCase, 'object');
+        assert.deepEqual(testCase, 'object');
     });
     it('should handle multiple spaces', () => {
-        let testcase = JSON.stringify(program.words('olly  olly car    car'));
-        assert.equal(testcase, JSON.stringify({ 'olly': 2, 'car': 2 }));
+        let testcase = program.words('olly  olly car    car');
+        assert.deepEqual(testcase, { 'olly': 2, 'car': 2 });
     });
     it('test that empty/space string return empty object', () => {
-        let testcase = JSON.stringify(program.words(''));
-        assert.equal(testcase, JSON.stringify({}));
-        testcase = JSON.stringify(program.words(' '));
-        assert.equal(testcase, JSON.stringify({}));
+        let testcase = program.words('');
+        assert.deepEqual(testcase, {});
+        testcase = program.words(' ');
+        assert.deepEqual(testcase, {});
     });
-    it('test that input is string', () => {
-        let testcase = JSON.stringify(program.words(6));
-        assert.equal(testcase, JSON.stringify({ 'error': 'parameter must be a string' }));
+    it('test that parameter must not be number', () => {
+        let testcase = program.words(6);
+        assert.deepEqual(testcase, { error: 'parameter must not be number' });
+    });
+    it('test that parameter must not be an array', () => {
+        let testcase = program.words([1, 2, 'wisdom']);
+        assert.deepEqual(testcase, { error: 'parameter must not be an array' });
+    });
+    it('test that parameter is not undefined', () => {
+        let testcase = program.words();
+        assert.deepEqual(testcase, { error: 'parameter is undefined' });
+    });
+    it('test that parameter must not be an object', () => {
+        let testcase = program.words({a:'apple', b:'boy'});
+        assert.deepEqual(testcase, { error: 'parameter must not be an object' });
     });
     it('should handle uppercase and lower case as same word', () => {
-        let testcase = JSON.stringify(program.words('wisdom  WisDom WISDOM'));
-        assert.equal(testcase, JSON.stringify({ 'wisdom': 3 }));
+        let testcase = program.words('wisdom  WisDom WISDOM');
+        assert.deepEqual(testcase, { 'wisdom': 3 });
     });
-    it('should handle empty parameter', () => {
-        let testcase = JSON.stringify(program.words());
-        assert.equal(testcase, JSON.stringify({ 'error': 'parameter must be a string' }));
-    });
-    it('should handle single word string', () => {
-        let testcase = JSON.stringify(program.words('car'));
-        assert.equal(testcase, JSON.stringify({ 'car': 1 }));
-    });
-    it('should handle unrepeated words', () => {
-        let testcase = JSON.stringify(program.words('fire on the mountain'));
-        assert.equal(testcase, JSON.stringify({ 'fire': 1, 'on': 1, 'the': 1, 'mountain': 1 }));
-    });
-
 });
