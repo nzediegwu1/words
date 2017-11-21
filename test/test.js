aa586696ed63de3eb644270e04d81973ca24311b
nzediegwu1 <nzediegwu1@outlook.com>
2017-11-21 09:29:48 +0100

    rephrase unit test descriptions and remove redundant tests *Implemented corrections specified by LFA in pull request *Removed unit test for more complex cases since the function was not designed to handle special characters

--- test/test.js
+++ test/test.js
@@ -2,25 +2,25 @@
 const chai = require('chai');
 const program = require('../words');
 const assert = chai.assert;
-chai.config.truncateThreshold = 0;
-describe('Test words function', () => {
-    it('should handle specified input', () => {
+describe('It should take string argument and count the occurrences of each word in the string', () => {
+
+    it('should return accurate json object when a sentence is supplied as argument', () => {
         let testcase = program.words('olly olly in come free');
         assert.deepEqual(testcase, { olly: 2, in: 1, come: 1, free: 1 });
-    });
-    it('should handle more complex input', () => {
-        let testcase = program.words('olly olly has come in the free has the free olly');
+
+        testcase = program.words('olly olly has come in the free has the free olly');
         assert.deepEqual(testcase, { 'olly': 3, 'has': 2, 'come': 1, 'in': 1, 'the': 2, 'free': 2 });
+
     });
     it('should return an object', () => {
         let testCase = typeof program.words('olly olly has come in the free has the free olly');
         assert.deepEqual(testCase, 'object');
     });
-    it('should handle multiple spaces', () => {
+    it('should strip off extra unwanted spaces in any input sentence/parameter', () => {
         let testcase = program.words('olly  olly car    car');
         assert.deepEqual(testcase, { 'olly': 2, 'car': 2 });
     });
-    it('test that empty/space string return empty object', () => {
+    it('should return an empty object when an empty string is supplied as the input parameter/sentence', () => {
         let testcase = program.words('');
         assert.deepEqual(testcase, {});
         testcase = program.words(' ');
@@ -39,7 +39,7 @@ describe('Test words function', () => {
         assert.deepEqual(testcase, { error: 'parameter is undefined' });
     });
     it('test that parameter must not be an object', () => {
-        let testcase = program.words({a:'apple', b:'boy'});
+        let testcase = program.words({ a: 'apple', b: 'boy' });
         assert.deepEqual(testcase, { error: 'parameter must not be an object' });
     });
     it('should handle uppercase and lower case as same word', () => {
